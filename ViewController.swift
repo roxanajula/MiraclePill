bf90e9931aa86ed4c936844526400c693049816b
Roxana Jula <roxanajula@Roxanas-MacBook-Pro.local>
2017-05-08 16:29:13 +0200

    Initial commit
    Code based on the iOS 10 & Swift 3 Course by Devslopes.

---
+++ ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Roxana Jula on 08/05/2017.
+//  Copyright © 2017 Roxana Jula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewAccessibilityDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func stateButtonPressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
+}
+
